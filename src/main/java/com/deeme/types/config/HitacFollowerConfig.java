a3242230284995a97e67c1067b86beff5d1a1dea
dm94 <dm94@hotmail.es>
2022-08-23 22:21:41 +0200

    v1.5.12 alpha 2 Added adicional condition to hitacfollower

--- src/main/java/com/deeme/types/config/HitacFollowerConfig.java
+++ src/main/java/com/deeme/types/config/HitacFollowerConfig.java
@@ -3,6 +3,8 @@ package com.deeme.types.config;
 import com.github.manolo8.darkbot.config.types.Option;
 
 public class HitacFollowerConfig {
+    @Option(value = "Enable")
+    public boolean enable = false;
     @Option(value = "Go to PVP Maps")
     public boolean goToPVP = true;
 }
