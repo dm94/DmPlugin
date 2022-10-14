2dcc9846dd742ca8395d1b2f0c181ed329e3be82
dm94 <dm94@hotmail.es>
2022-10-14 17:51:25 +0200

    Changed maximum range for sentinel module

--- src/main/java/com/deeme/types/config/SentinelConfig.java
+++ src/main/java/com/deeme/types/config/SentinelConfig.java
@@ -1,46 +1,51 @@
 package com.deeme.types.config;
 
 import com.github.manolo8.darkbot.config.PlayerTag;
-import com.github.manolo8.darkbot.config.types.Num;
-import com.github.manolo8.darkbot.config.types.Option;
 import com.github.manolo8.darkbot.config.types.Tag;
 import com.github.manolo8.darkbot.config.types.TagDefault;
 
+import eu.darkbot.api.config.annotations.Configuration;
+import eu.darkbot.api.config.annotations.Number;
+import eu.darkbot.api.config.annotations.Option;
+
+@Configuration("sentinel")
 public class SentinelConfig {
-    @Option(value = "Sentinel ID", description = "Main priority. Can be empty, will use tag or group leader")
-    @Num(max = 2000000000, step = 1)
+    @Option(value = "sentinel.master_id")
+    @Number(max = 2000000000, step = 1)
     public int MASTER_ID = 0;
 
-    @Option(value = "Sentinel Tag", description = "Medium priority. He'll follow every ship with that tag")
+    @Option(value = "sentinel.tag")
     @Tag(TagDefault.ALL)
     public PlayerTag SENTINEL_TAG = null;
 
-    @Option(value = "Follow group leader", description = "Last priority. Follow the leader of the group")
+    @Option(value = "sentinel.follow_group")
     public boolean followGroupLeader = true;
 
-    @Option(value = "Ignore security", description = "Ignore the config, when enabled, the ship will follow you to death.")
+    @Option(value = "sentinel.ignore_safety")
     public boolean ignoreSecurity = false;
 
-    @Option(value = "Range towards the leader", description = "Distance it will be from the leader")
-    @Num(min = 10, max = 1000, step = 100)
+    @Option(value = "sentinel.range_to_leader")
+    @Number(min = 10, step = 100, max = 100000)
     public int rangeToLider = 300;
 
-    @Option(value = "Enable collector", description = "It will use the collector module when it is not doing anything.")
+    @Option(value = "sentinel.collector")
     public boolean collectorActive = false;
 
-    @Option(value = "Copy master formation", description = "It will try to use the master's formation")
+    @Option(value = "sentinel.copy_master_formation")
     public boolean copyMasterFormation = false;
 
-    @Option(value = "Jumping through portals (New logic)", description = "If the sentinel disappears near a portal, it will jump. It's faster than waiting for the group info")
+    @Option(value = "sentinel.jumping_portals")
     public boolean followByPortals = false;
 
-    @Option(value = "Move to the location the master is going to", description = "It will go to the master's destination and not to where the master is.")
-    public boolean goToMasterDestination = false;
+    @Option(value = "sentinel.use_master_destination")
+    public boolean goToMasterDestination = true;
 
-    @Option(value = "Aggressive follow up", description = "Always respect the following range to the leader. This will make the bot less human")
+    @Option(value = "sentinel.aggressive_follow")
     public boolean aggressiveFollow = false;
 
-    public @Option(value = "Auto Attack", description = "Will attack even when the master is not attacking") AutoAttack autoAttack = new AutoAttack();
+    @Option(value = "sentinel.auto_attack")
+    public AutoAttack autoAttack = new AutoAttack();
 
-    public @Option(value = "Auto Cloak", description = "It will automatically camouflage") AutoCloak autoCloak = new AutoCloak();
+    @Option(value = "general.auto_cloak")
+    public AutoCloak autoCloak = new AutoCloak();
 }
