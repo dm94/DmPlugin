ac3ed1c83a3e8310aa00e1c0cb7fd77dba9c49dd
Dm94 <dm94@hotmail.es>
2022-10-29 19:22:26 +0200

    Updated best rocket to display the missile name

--- src/main/java/com/deeme/behaviours/bestrocket/BestRocketConfig.java
+++ src/main/java/com/deeme/behaviours/bestrocket/BestRocketConfig.java
@@ -1,18 +1,13 @@
 package com.deeme.behaviours.bestrocket;
 
-import com.deeme.types.gui.RocketSupplier;
-import com.github.manolo8.darkbot.config.types.Editor;
-import com.github.manolo8.darkbot.config.types.Options;
-import com.github.manolo8.darkbot.gui.tree.components.JListField;
-
 import eu.darkbot.api.config.annotations.Configuration;
+import eu.darkbot.api.config.annotations.Dropdown;
 import eu.darkbot.api.config.annotations.Option;
 
 @Configuration("best_rocket")
 public class BestRocketConfig {
     @Option(value = "best_rocket.npc_rocket")
-    @Editor(JListField.class)
-    @Options(RocketSupplier.class)
+    @Dropdown(options = RocketSupplier.class)
     public String npcRocket = "";
 
     @Option(value = "best_rocket.useicr")
