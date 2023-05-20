037356691ce1a3bfcb8ef78f74e7a7cecd14f0d7
Dm94 <dm94@hotmail.es>
2023-05-20 12:42:56 +0200

    Updated pvp module translations

--- src/main/java/com/deeme/modules/pvp/PVPConfig.java
+++ src/main/java/com/deeme/modules/pvp/PVPConfig.java
@@ -2,49 +2,59 @@ package com.deeme.modules.pvp;
 
 import com.deeme.types.config.AutoCloak;
 import com.deeme.types.config.ExtraKeyConditions;
-import com.github.manolo8.darkbot.config.Config;
-import com.github.manolo8.darkbot.config.types.Num;
-import com.github.manolo8.darkbot.config.types.Option;
 
-@Option("PVP Config")
+import com.github.manolo8.darkbot.config.Config.Loot.Sab;
+
+import eu.darkbot.api.config.annotations.Configuration;
+import eu.darkbot.api.config.annotations.Number;
+import eu.darkbot.api.config.annotations.Option;
+
+@Configuration("PVP Config")
 public class PVPConfig {
-    @Option(value = "Movement", description = "The ship will move")
+    @Option("pvp_module.movement")
     public boolean move = true;
 
-    @Option(value = "Recharge shields", description = "Recharge the shield for both Attack and Run configurations")
+    @Option("pvp_module.recharge_shields")
     public boolean rechargeShields = true;
 
-    @Option(value = "Enable collector", description = "It will use the collector module when it is not doing anything.")
+    @Option("pvp_module.enable_collector")
     public boolean collectorActive = false;
 
-    @Option(value = "Auto change config", description = "It will change configuration automatically")
+    @Option("pvp_module.change_config")
     public boolean changeConfig = true;
 
-    @Option(value = "Use the run configuration", description = "Will use the run setting if enemies flee")
+    @Option("pvp_module.run_config")
     public boolean useRunConfig = true;
 
-    @Option(value = "Maximum range for enemies", description = "Enemies above this range will not be attacked")
-    @Num(min = 0, max = 1000, step = 50)
+    @Option("auto_attack.max_range")
+    @Number(min = 0, max = 1000, step = 50)
     public int rangeForEnemies = 500;
 
-    @Option(value = "Maximum range for the enemy attacked", description = "Above this range the enemy is considered to have escaped.")
-    @Num(min = 1000, max = 4000, step = 100)
+    @Option("pvp_module.max_range_enemy_attacked")
+    @Number(min = 1000, max = 4000, step = 100)
     public int rangeForAttackedEnemy = 2000;
 
-    @Option(value = "RCB-140 | RSB-75", description = "Use RCB-140 | RSB-75")
+    @Option("general.rsb")
     public boolean useRSB = false;
 
-    public @Option(key = "config.loot.sab") Config.Loot.Sab SAB = new Config.Loot.Sab();
+    @Option("config.loot.sab")
+    public Sab SAB = new Sab();
 
-    public @Option(value = "Ability", description = "Ability Conditions") ExtraKeyConditions ability = new ExtraKeyConditions();
+    @Option("general.ability")
+    public ExtraKeyConditions ability = new ExtraKeyConditions();
 
-    public @Option(value = "ISH-01", description = "ISH-01 Conditions") ExtraKeyConditions ISH = new ExtraKeyConditions();
+    @Option("general.ish")
+    public ExtraKeyConditions ISH = new ExtraKeyConditions();
 
-    public @Option(value = "SMB-01", description = "SMB-01 Conditions") ExtraKeyConditions SMB = new ExtraKeyConditions();
+    @Option("general.smb")
+    public ExtraKeyConditions SMB = new ExtraKeyConditions();
 
-    public @Option(value = "PEM-01", description = "PEM-01 Conditions") ExtraKeyConditions PEM = new ExtraKeyConditions();
+    @Option("general.pem")
+    public ExtraKeyConditions PEM = new ExtraKeyConditions();
 
-    public @Option(value = "Auto Cloak", description = "It will automatically camouflage") AutoCloak autoCloak = new AutoCloak();
+    @Option("general.auto_cloak")
+    public AutoCloak autoCloak = new AutoCloak();
 
-    public @Option(value = "Anti Push") AntiPush antiPush = new AntiPush();
+    @Option("anti_push")
+    public AntiPush antiPush = new AntiPush();
 }
