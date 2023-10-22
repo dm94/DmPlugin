bebce8c8df72b7d1a89e3e3587e022b5d4f34e6a
Daniel Martín <dm94@hotmail.es>
2023-10-22 13:33:05 +0200

    Feat/gradle (#92)
    * First test with gradle build
    
    * Update the build and some parts to export everything propertly

---
+++ build.gradle.kts
@@ -0,0 +1,57 @@
+
+plugins {
+    id("org.gradle.java-library")
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.guardsquare", "proguard-gradle", "7.2.2")
+    }
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+
+    maven { url = uri("https://jitpack.io") }
+}
+
+allprojects {
+    java {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+group = "com.deeme"
+version = "2.1.10"
+description = "DmPlugin"
+
+dependencies {
+    api("eu.darkbot.DarkBotAPI", "darkbot-impl", "0.7.8")
+    api("eu.darkbot", "DarkBot", "6d026357cb")
+    implementation(files("private.jar"))
+}
+
+tasks.register<proguard.gradle.ProGuardTask>("proguard") {
+    dependsOn("uberJar")
+    configuration("proguard.conf")
+    dontnote()
+    dontwarn()
+
+    injars("./build/libs/DmPlugin-2.1.10.jar")
+    outjars("DmPlugin.jar")
+}
+
+tasks.register<Jar>("uberJar") {
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.equals("private.jar") }.map { zipTree(it) }
+    })
+}
