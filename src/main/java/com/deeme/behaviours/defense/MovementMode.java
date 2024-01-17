c395e4589e4bcc30a04b57e34fcaebfb2dcaa003
Daniel Martín <dm94@hotmail.es>
2024-01-17 22:20:11 +0100

    fix: Uploaded an empty version of private so that the build can be made (#118)

--- src/main/java/com/deeme/behaviours/defense/MovementMode.java
+++
@@ -1,8 +0,0 @@
-package com.deeme.behaviours.defense;
-
-import eu.darkbot.api.config.annotations.Configuration;
-
-@Configuration("defense.movement_mode.list")
-public enum MovementMode {
-    VS, VSSAFETY, RANDOM, GROUPVSSAFETY, GROUPVS;
-}
