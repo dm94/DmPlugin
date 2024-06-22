0dc80edfbcf73da38b21caff4882847ee97eba11
Daniel Martín <dm94@hotmail.es>
2024-06-22 16:57:36 +0200

    feat: QuestModule: Spend amunition quests will now be marked for attacking NPCs. (#156)

--- src/main/java/com/deeme/modules/quest/QuestModuleDummy.java
+++ src/main/java/com/deeme/modules/quest/QuestModuleDummy.java
@@ -2,7 +2,6 @@ package com.deeme.modules.quest;
 
 import eu.darkbot.api.managers.AuthAPI;
 import eu.darkbot.api.managers.ExtensionsAPI;
-import eu.darkbot.api.utils.Inject;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -23,19 +22,16 @@ import eu.darkbot.api.extensions.InstructionProvider;
 public class QuestModuleDummy extends QuestModule implements InstructionProvider, ExtraMenus {
     private JLabel label = new JLabel("");
 
-    public QuestModuleDummy(PluginAPI api) {
-        this(api, api.requireAPI(AuthAPI.class));
-    }
-
-    @Inject
-    public QuestModuleDummy(PluginAPI api, AuthAPI auth) {
+    public QuestModuleDummy(PluginAPI api) throws SecurityException {
         super(api);
 
+        AuthAPI auth = api.requireAPI(AuthAPI.class);
         if (!Arrays.equals(VerifierChecker.class.getSigners(), getClass().getSigners())) {
             throw new SecurityException();
         }
 
         VerifierChecker.requireAuthenticity(auth);
+
         ExtensionsAPI extensionsAPI = api.requireAPI(ExtensionsAPI.class);
         Utils.discordDonorCheck(extensionsAPI.getFeatureInfo(this.getClass()), auth.getAuthId());
 
