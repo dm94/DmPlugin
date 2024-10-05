3a8d9d215f4f0cc0f907ed8ecc09854fc32664d6
dm94 <dm94@hotmail.es>
2024-10-05 15:42:59 +0200

    feat: The old Urgent detector will now give an error if you want to use it

--- src/main/java/com/deeme/behaviours/UrgentDetectorDummy.java
+++ src/main/java/com/deeme/behaviours/UrgentDetectorDummy.java
@@ -1,28 +1,11 @@
 package com.deeme.behaviours;
 
-import java.util.Arrays;
-
-import com.deeme.behaviours.urgentdetector.UrgentDetector;
-import com.deeme.types.VerifierChecker;
-import com.deeme.types.backpage.Utils;
-
 import eu.darkbot.api.PluginAPI;
 import eu.darkbot.api.extensions.Feature;
-import eu.darkbot.api.managers.AuthAPI;
-import eu.darkbot.api.managers.ExtensionsAPI;
 
 @Feature(name = "Urgent Detector [OLD]", description = "Use the other Urgent Detector")
-public class UrgentDetectorDummy extends UrgentDetector {
+public class UrgentDetectorDummy {
     public UrgentDetectorDummy(PluginAPI api) throws SecurityException {
-        super(api);
-
-        if (!Arrays.equals(VerifierChecker.class.getSigners(), getClass().getSigners())) {
-            throw new SecurityException();
-        }
-
-        AuthAPI auth = api.requireAPI(AuthAPI.class);
-        VerifierChecker.requireAuthenticity(auth);
-        ExtensionsAPI extensionsAPI = api.requireAPI(ExtensionsAPI.class);
-        Utils.discordDonorCheck(extensionsAPI.getFeatureInfo(this.getClass()), auth.getAuthId());
+        throw new UnsupportedOperationException("Use the other Urgent Detector, this one will be deleted");
     }
 }
